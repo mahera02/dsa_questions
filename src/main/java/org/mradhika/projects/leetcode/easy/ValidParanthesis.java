82f3014998af061b4f0b523beab535e41cd757b1
radhi <radhika4cse@gmail.com>
2019-01-23 17:27:24 -0500

    valid paranthesis

---
+++ src/main/java/org/mradhika/projects/leetcode/easy/ValidParanthesis.java
@@ -0,0 +1,71 @@
+package org.mradhika.projects.leetcode.easy;
+
+import java.util.HashSet;
+import java.util.Stack;
+
+public class ValidParanthesis {
+
+	/*
+	Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+	An input string is valid if:
+
+	Open brackets must be closed by the same type of brackets.
+	Open brackets must be closed in the correct order.
+	Note that an empty string is also considered valid.
+	*/
+	
+	//first attempt
+	public static boolean isValid(String s) {
+		if (s.isEmpty()) {
+			return true;
+		}
+		boolean isValid = true;
+		char[] ch = s.toCharArray();
+		Stack<Character> paran = new Stack<>();
+		if (ch.length == 1) {
+			return false;
+		}
+		for (int i = 0; i < ch.length; i++) {
+			if (ch[i] == '(' || ch[i] == '{' || ch[i] == '[') {
+				paran.push(ch[i]);
+				isValid = false;
+			} else {
+				char top = 0;
+				if (!paran.isEmpty()) {
+					top = paran.pop().charValue();
+					isValid = true;
+				} else {
+					isValid = false;
+					break;
+				}
+				if (ch[i] == ')' && top != '(') {
+					isValid = false;
+					break;
+				}
+				if (ch[i] == '}' && top != '{') {
+					isValid = false;
+					break;
+				}
+
+				if (ch[i] == ']' && top != '[') {
+					isValid = false;
+					break;
+				}
+			}
+		}
+		if (paran.isEmpty() && isValid)
+			return true;
+		else
+			return false;
+
+	}
+
+	public static void main(String[] args) {
+
+		boolean result = ValidParanthesis.isValid("([]");
+		System.out.println(result);
+
+	}
+
+}
