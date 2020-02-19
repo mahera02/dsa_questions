f78125ff54313afeae0d6010ac77ac28c7e465ea
radhi <radhika4cse@gmail.com>
2020-02-19 15:07:00 -0800

    first bad version

---
+++ src/main/java/org/mradhika/projects/leetcode/easy/FirstBadVersion.java
@@ -0,0 +1,27 @@
+package org.mradhika.projects.leetcode.easy;
+
+public class FirstBadVersion extends VersionControl{
+
+	/* The isBadVersion API is defined in the parent class VersionControl.
+    boolean isBadVersion(int version); */
+
+  public int firstBadVersion(int n) {
+      
+      int left = 1;
+      int right = n;
+      
+      while(left <right){
+          int mid = left + (right-left)/2;
+          
+          if(isBadVersion(mid))
+              right = mid;
+          
+          else
+              left = mid+1;
+      }
+      
+      return left;
+  }
+
+
+}
---
+++ src/main/java/org/mradhika/projects/leetcode/easy/VersionControl.java
@@ -0,0 +1,9 @@
+package org.mradhika.projects.leetcode.easy;
+
+public class VersionControl {
+	
+	public boolean isBadVersion(int mid) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+}
