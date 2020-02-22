0ed10fcc3592993e955ed33d92439850c4dddb5f
radhi <radhika4cse@gmail.com>
2020-02-21 23:57:35 -0800

    practice

---
+++ src/main/java/org/mradhika/projects/leetcode/easy/LinkListCycle.java
@@ -0,0 +1,32 @@
+package org.mradhika.projects.leetcode.easy;
+
+public class LinkListCycle {
+
+	/**
+	 * Definition for singly-linked list.
+	 * class ListNode {
+	 *     int val;
+	 *     ListNode next;
+	 *     ListNode(int x) {
+	 *         val = x;
+	 *         next = null;
+	 *     }
+	 * }
+	 */
+	    public boolean hasCycle(ListNode head) {
+	    	
+	    	if(head==null)
+	    		return false;
+	    	
+	    	ListNode slow = head;
+	    	ListNode fast = head.next;
+	    	
+	    	while(fast!=null && fast.next!=null) {
+	    		slow= slow.next;
+	    		fast=fast.next.next;
+	    		if(slow==fast)
+	    			return true;
+	    	}
+			return false;
+	    }
+}
