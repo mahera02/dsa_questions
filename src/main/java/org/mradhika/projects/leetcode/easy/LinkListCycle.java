5e9028ee9aeb2f24e176e2531aa13423fe6ae240
radhi <radhika4cse@gmail.com>
2020-02-28 23:30:01 -0800

    update

--- src/main/java/org/mradhika/projects/leetcode/easy/LinkListCycle.java
+++ src/main/java/org/mradhika/projects/leetcode/easy/LinkListCycle.java
@@ -3,30 +3,43 @@ package org.mradhika.projects.leetcode.easy;
 public class LinkListCycle {
 
 	/**
-	 * Definition for singly-linked list.
-	 * class ListNode {
-	 *     int val;
-	 *     ListNode next;
-	 *     ListNode(int x) {
-	 *         val = x;
-	 *         next = null;
-	 *     }
-	 * }
+	 * Definition for singly-linked list. class ListNode { int val; ListNode next;
+	 * ListNode(int x) { val = x; next = null; } }
 	 */
-	    public boolean hasCycle(ListNode head) {
-	    	
-	    	if(head==null)
-	    		return false;
-	    	
-	    	ListNode slow = head;
-	    	ListNode fast = head.next;
-	    	
-	    	while(fast!=null && fast.next!=null) {
-	    		slow= slow.next;
-	    		fast=fast.next.next;
-	    		if(slow==fast)
-	    			return true;
-	    	}
+	public boolean hasCycle(ListNode head) {
+
+		if (head == null)
 			return false;
-	    }
+
+		ListNode slow = head;
+		ListNode fast = head.next;
+
+		while (fast != null && fast.next != null) {
+			slow = slow.next;
+			fast = fast.next.next;
+			if (slow == fast)
+				return true;
+		}
+		return false;
+	}
+
+	// LinkListCycle 2
+	public ListNode detectCycle(ListNode head) {
+
+		ListNode slow = head;
+		ListNode fast = head;
+		while (fast != null && fast.next != null) {
+			slow = slow.next;
+			fast = fast.next.next;
+			if (slow == fast) {
+				slow = head;
+				while (slow != fast) {
+					slow = slow.next;
+					fast = fast.next;
+				}
+				return slow;
+			}
+		}
+		return null;
+	}
 }
