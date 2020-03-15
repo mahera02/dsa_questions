01b23bbcbdc1a85bd6a153c328b131b6c0b540a9
radhi <radhika4cse@gmail.com>
2020-03-14 20:27:21 -0700

    intersection

--- src/main/java/org/mradhika/projects/leetcode/easy/IntersectionOfTwoArrays.java
+++ src/main/java/org/mradhika/projects/leetcode/easy/IntersectionOfTwoArrays.java
@@ -1,6 +1,6 @@
 package org.mradhika.projects.leetcode.easy;
 
-import java.util.ArrayList;
+import java.util.*;
 import java.util.HashSet;
 
 import javax.print.attribute.HashAttributeSet;
@@ -33,5 +33,44 @@ public class IntersectionOfTwoArrays {
 		}
 		return result;
 	}
+	 public int[] intersectionBinarySearch(int[] nums1, int[] nums2) {
+	        
+	        Arrays.sort(nums2);
+	        Set<Integer> set= new HashSet<>();
+	        for(int n : nums1){
+	           if(binarySearch(nums2,n)){
+	               set.add(n);
+	           }
+	        }
+	        int[] res = new int[set.size()];
+	        int k = 0;
+	        for(int i : set){
+	            res[k] = i;
+	            k++;
+	            
+	        }
+	        return res;
+	    }
+	    
+	    public boolean binarySearch(int[] nums, int target){
+	        
+	        int left = 0;
+	        int right = nums.length-1;
+	       
+	        while(left<=right){
+	            int mid = left +(right-left)/2;
+	            
+	            if(nums[mid]==target)
+	                return true;
+	            else if (nums[mid] > target){
+	                right = mid-1;
+	            }
+	            else{
+	                left = mid+1;
+	            }
+	        }
+	        return false;
+	        
+	    }
 
 }
