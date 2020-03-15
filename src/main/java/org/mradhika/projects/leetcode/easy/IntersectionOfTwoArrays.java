21904adec5f852ece9110ee1dec9d0b28b6bd08c
radhi <radhika4cse@gmail.com>
2020-03-14 20:11:07 -0700

    intersection of arrays

---
+++ src/main/java/org/mradhika/projects/leetcode/easy/IntersectionOfTwoArrays.java
@@ -0,0 +1,37 @@
+package org.mradhika.projects.leetcode.easy;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+
+import javax.print.attribute.HashAttributeSet;
+
+public class IntersectionOfTwoArrays {
+
+	public static void main(String[] args) {
+	   int[] response=	IntersectionOfTwoArrays.intersection(new int[] {1,2,2,1}, new int[] {2,2});
+	   System.out.println(response[0]);
+	   System.out.println(response.length);
+	}
+
+	public static int[] intersection(int[] nums1, int[] nums2) {
+
+		HashSet<Integer> set = new HashSet<>();
+
+		for (int num : nums1) {
+			set.add(num);
+		}
+
+		ArrayList<Integer> list = new ArrayList<>();
+		for (int num : nums2) {
+			if (set.remove(num)) {
+				list.add(num);
+			}
+		}
+		int[] result = new int[list.size()];
+		for (int i = 0; i < list.size(); i++) {
+			result[i] = list.get(i);
+		}
+		return result;
+	}
+
+}
