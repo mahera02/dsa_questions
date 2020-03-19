0b01ecf3a039b3f7a27b19d3db60a111fa19a6da
radhi <radhika4cse@gmail.com>
2020-03-19 00:12:49 -0700

    hamming Distance

---
+++ src/main/java/org/mradhika/projects/leetcode/easy/HammingDistance.java
@@ -0,0 +1,19 @@
+package org.mradhika.projects.leetcode.easy;
+
+public class HammingDistance {
+	 public static int hammingDistance(int x, int y) {
+	        int z= x ^ y;
+	        int countOfSetBits=0;
+	        while (z != 0) {
+	            countOfSetBits += z & 1;
+	            z = z >> 1;
+	        }
+	     return countOfSetBits;
+	   }
+	 
+	 public static void main(String[] args) {
+		
+		 HammingDistance.hammingDistance(1,4);
+		 
+	}
+}
