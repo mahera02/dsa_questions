182793765fac84fdff91f64f2cfa7e2af1c40f0b
radhi <radhika4cse@gmail.com>
2020-03-29 14:36:00 -0700

    n ary

--- src/main/java/org/mradhika/projects/leetcode/easy/MaxDepthNaryTree.java
+++ src/main/java/org/mradhika/projects/leetcode/easy/MaxDepthNaryTree.java
@@ -1,10 +1,33 @@
 package org.mradhika.projects.leetcode.easy;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 
 public class MaxDepthNaryTree {
-	public int maxDepthDFS(Node root) {
+	
+	public static void main(String[] args) {
+		
+		Node n5 = new Node(5, new ArrayList<>());
+		Node n6 = new Node(6, new ArrayList<>());
+		List<Node> childrenN2 = new ArrayList<>();
+		childrenN2.add(n5);
+		childrenN2.add(n6);
+		Node n2 = new Node(3,childrenN2);
+		Node n3 = new Node(2, new ArrayList<>());
+		Node n4 = new Node(4, new ArrayList<>());
+		List<Node> childrenN1 = new ArrayList<>();
+		childrenN1.add(n2);
+		childrenN1.add(n3);
+		childrenN1.add(n4);
+		Node n1 = new Node(1,childrenN1);
+		
+		 int res = MaxDepthNaryTree.maxDepthDFS(n1);
+		 System.out.println(res);
+	}
+	
+	public static int maxDepthDFS(Node root) {
 
 		if (root == null)
 			return 0;
