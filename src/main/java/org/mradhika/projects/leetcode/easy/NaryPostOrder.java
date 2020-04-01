e7c7bd3ee56bdb4f4c04b31e72b5cc9903586ac1
radhi <radhika4cse@gmail.com>
2020-04-01 13:59:54 -0700

    n -ary iterative

--- src/main/java/org/mradhika/projects/leetcode/easy/NaryPostOrder.java
+++ src/main/java/org/mradhika/projects/leetcode/easy/NaryPostOrder.java
@@ -1,7 +1,9 @@
 package org.mradhika.projects.leetcode.easy;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Stack;
 
 public class NaryPostOrder {
 	   public List<Integer> postorder(Node root) {
@@ -19,5 +21,25 @@ public class NaryPostOrder {
 	        }
 	        list.add(node.val);
 	    
+	    }
+	    
+	    //Iterative
+	    public List<Integer> postorderIterative(Node root) {
+	         List<Integer>  list  = new ArrayList<>();
+		        if(root==null)
+		            return list;
+		        Stack<Node> stack = new Stack<>();
+		        stack.push(root);
+		        
+		        while(!stack.isEmpty()){
+		            Node interim = stack.pop();
+		             list.add(interim.val);
+	                for(int i = 0 ; i<interim.children.size() ;i++){
+		                stack.push(interim.children.get(i));
+		            }
+		        }
+	        Collections.reverse(list);
+		        return list;
+		        
 	    }
 }
--- src/main/java/org/mradhika/projects/leetcode/easy/NaryPreOrder.java
+++ src/main/java/org/mradhika/projects/leetcode/easy/NaryPreOrder.java
@@ -2,6 +2,7 @@ package org.mradhika.projects.leetcode.easy;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 public class NaryPreOrder {
 	  public List<Integer> preorder(Node root) {
@@ -19,6 +20,31 @@ public class NaryPreOrder {
 	        for(Node child : node.children){
 	             preOrderList(list,child);
 	        }
+	        
+	    }
+	    
+//	    Iterative
+	    
+	    public List<Integer> preorderIterative(Node root) {
+	         List<Integer>  list  = new ArrayList<>();
+	         
+	        if(root==null)
+	            return list;
+	        Stack<Node> stack = new Stack<>();
+	        stack.push(root);
+	        
+	        while(!stack.isEmpty()){
+	            Node interim = stack.pop();
+	            list.add(interim.val);
+	            
+	            for(int i = interim.children.size()-1 ; i>=0 ;i--){
+	                stack.push(interim.children.get(i));
+	            }
+	            
+	            
+	        }
+	        return list;
+	        
 	        
 	    }
 }
