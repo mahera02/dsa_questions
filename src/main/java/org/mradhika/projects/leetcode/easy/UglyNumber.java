aaea4f29039cfd89a8a937a58661519eb9f0b03d
radhi <radhika4cse@gmail.com>
2020-04-02 15:03:42 -0700

    ugly num

---
+++ src/main/java/org/mradhika/projects/leetcode/easy/UglyNumber.java
@@ -0,0 +1,28 @@
+package org.mradhika.projects.leetcode.easy;
+
+public class UglyNumber {
+	 public boolean isUgly(int num) {
+	        
+	        if(num<=0)
+	            return false;
+	        boolean isUgly = true;
+	        while(num>1){
+	            
+	            if(num%2==0){
+	                num=num/2;
+	            }
+	            else if(num%3==0)
+	                 num=num/3;
+	            
+	            else if(num%5==0)
+	                 num=num/5;
+	            
+	            else{
+	                isUgly= false;
+	                break;
+	            }
+	        }
+	        
+	        return isUgly;
+	    }
+}
